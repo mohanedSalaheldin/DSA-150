205d4f0c0b6a01364277fa1392ff9e27d4587a7b
mohanedSalaheldin <mohnsd1162002@gmail.com>
2024-12-13 21:02:09 +0200

    TRY: build BT from Preorder & Inorder

--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class TreeProblems {
     public TreeNode searchBST(TreeNode root, int val) {
         if(root==null) return null;
@@ -18,6 +20,29 @@ public class TreeProblems {
         if(cur>p.val && cur>q.val) return lowestCommonAncestor( root.left, p, q);
         if(cur<p.val && cur<q.val) return lowestCommonAncestor( root.right, p, q);
         return root;
+    }
+
+
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        int count = 0;
+
+        while (cur!=null || !stack.isEmpty()){
+            while (cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            count++;
+            if (count==k) return cur.val;
+            cur = cur.right;
+        }
+        return -1;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
     }
 }
 
