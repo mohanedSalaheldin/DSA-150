54df836909281fb887e919a23a69376fbb87b2c4
mohanedSalaheldin <mohnsd1162002@gmail.com>
2024-12-19 22:10:37 +0200

    back to graphs

--- src/recursion/Maps.java
+++ src/recursion/Maps.java
@@ -36,29 +36,37 @@ public class Maps {
 
 class LRUCache {
     int capacity;
+    ArrayList<Integer> usage;
     Map<Integer, Integer> cache;
     public LRUCache(int capacity) {
         this.capacity = capacity;
         cache = new HashMap<>();
+        usage = new ArrayList<>() {
+        };
     }
 
     public int get(int key) {
-        if(cache.containsKey(key))
+        if(cache.containsKey(key)){
+            if (usage.contains(key))
+                usage.remove(key);
+            usage.add(key);
             return cache.get(key);
+        }
         return -1;
     }
 
     public void put(int key, int value) {
         if(cache.containsKey(key)){
-            int val = cache.get(key);
-            cache.
+            cache.put(key, value);
         }else {
             if (cache.size()>=capacity){
-
+                int leastIxd = usage.get(0);
+                usage.remove(0);
+                cache.remove(leastIxd);
             }else {
-                cache.put(key,value);
                 capacity++;
             }
+            cache.put(key,value);
         }
     }
 }
